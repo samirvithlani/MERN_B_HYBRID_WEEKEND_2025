2867db065216c340dea35a2c1b13ba8db0a0d6ec
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-18 13:32:25 +0530

    depeddropdown

--- src/components/input/DepedentDropdown.jsx
+++ src/components/input/DepedentDropdown.jsx
@@ -1,43 +1,98 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export const DepedentDropdown = () => {
-
-    const [selectedCountry, setselectedCountry] = useState("")
-    var data = [
+  const [selectedCountry, setselectedCountry] = useState("");
+  const [selectedCity, setselectedCity] = useState("");
+  const [selectedState, setselectedState] = useState("");
+  // var data = [
+  //     {
+  //         country:"india",
+  //         states:["Gujarat","Maharashtra"]
+  //     },
+  //     {
+  //         country:"USA",
+  //         states:["WD","California"]
+  //     }
+  // ]
+  var data = [
+    {
+      country: "india",
+      states: [
         {
-            country:"india",
-            states:["Gujarat","Maharashtra"]
+          name: "Gujarat",
+          city: ["Ahm", "Surat"],
         },
         {
-            country:"USA",
-            states:["WD","California"]
-        }
-    ]
-    const countryHanlder = (event)=>{
-        console.log(event.target.value)
-        setselectedCountry(event.target.value)
-    }
+          name: "Mah",
+          city: ["Mumbai", "Pune"],
+        },
+      ],
+    },
+    {
+      country: "USA",
+      states: [
+        {
+          name: "WD",
+          city: ["Abc", "Pqr"],
+        },
+      ],
+    },
+  ];
+  const countryHanlder = (event) => {
+    console.log(event.target.value);
+    setselectedCountry(event.target.value);
+  };
+  const stateHandler = (event) => {
+    setselectedState(event.target.value);
+  };
+  const cityHandler = (event) => {
+    setselectedCity(event.target.value);
+  };
   return (
-    <div style={{textAlign:"center"}}>
-        <h1>DEPEDENT DROPDOWN</h1>
+    <div style={{ textAlign: "center" }}>
+      <h1>DEPEDENT DROPDOWN</h1>
 
-        <select onChange={(event)=>{countryHanlder(event)}}>
-            <option>SELECT COUNTRY</option>
-            {
-                data.map((d)=>{
-                    return<option value={d.country}>{d.country}</option>
-                })
-            }
-        </select>
-        <h4>{selectedCountry}</h4>
-        <select>
-            <option>SELECT STATE</option>
-            {
-                selectedCountry && data.find((d)=>d.country==selectedCountry).states.map((s)=>{
-                    return <option>{s}</option>
-                })
-            }
-        </select>
+      <select
+        onChange={(event) => {
+          countryHanlder(event);
+        }}
+      >
+        <option>SELECT COUNTRY</option>
+        {data.map((d) => {
+          return <option value={d.country}>{d.country}</option>;
+        })}
+      </select>
+
+      <h4>{selectedCountry}</h4>
+      <select
+        onChange={(event) => {
+          stateHandler(event);
+        }}
+      >
+        <option>SELECT STATE</option>
+        {selectedCountry &&
+          data
+            .find((d) => d.country == selectedCountry)
+            .states.map((s) => {
+              return <option>{s.name}</option>;
+            })}
+      </select>
+      {selectedState}
+      <select
+        onChange={(event) => {
+          cityHandler(event);
+        }}
+      >
+        <option>SELECT CITY</option>
+        {selectedState &&
+          data
+            .find((c) => c.country == selectedCountry)
+            .states.find((s) => s.name == selectedState)
+            .city.map((c) => {
+              return <option>{c}</option>;
+            })}
+      </select>
+      {selectedCity}
     </div>
-  )
-}
+  );
+};
