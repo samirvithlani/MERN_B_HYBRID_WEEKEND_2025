6602e54b0782a200bf6edc9e4abc24a052661347
samirvithlani <samir.vithlani83955@gmail.com>
2026-02-15 13:54:18 +0530

    api calling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -88,8 +88,11 @@ export const Navbar = () => {
                 formdemo6
               </Link>
             </li>
-            
-          
+            <li class="nav-item active">
+              <Link class="nav-link" to="/apidemo1">
+                apidemo1
+              </Link>
+            </li>
           </ul>
         </div>
       </nav>
---
+++ src/components/api/ApiDemo1.jsx
@@ -0,0 +1,40 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+
+export const ApiDemo1 = () => {
+
+  const [message, setmessage] = useState("")
+  const [users, setusers] = useState([])
+
+  //GET API --> backend ->
+  const getUser = async()=>{
+    //get --><promise<Axios
+    //response -->datatype ->proimse
+    const response = await axios.get("https://node5.onrender.com/user/user/")
+    console.log(response) //promise object then catch [async await]
+    //response -->axios object 
+    //api reponse ---> response.data
+    console.log(response.data) // api response..
+    console.log(response.data.message)
+    //set
+    setmessage(response.data.message)
+    console.log(response.data.data)
+    setusers(response.data.data)
+    
+
+  }
+
+  return (
+    <div style={{textAlign:"center"}}>
+        <h1>API DEMO 1</h1>
+        <button onClick={()=>{getUser()}}>GET</button>
+        {message}
+        {
+          users.map((user)=>{
+            return <li>{user.name}</li>
+          })
+        }
+       
+    </div>
+  )
+}
