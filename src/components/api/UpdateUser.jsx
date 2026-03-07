d12cd3c5a32d85568cdce57233d2edd7be9da4be
samirvithlani <samir.vithlani83955@gmail.com>
2026-03-07 18:53:21 +0530

    udpate

--- src/components/api/UpdateUser.jsx
+++ src/components/api/UpdateUser.jsx
@@ -1,17 +1,27 @@
 import axios from 'axios'
 import React from 'react'
 import { useForm } from 'react-hook-form'
-import { data, useParams } from 'react-router-dom'
+import { data, useNavigate, useParams } from 'react-router-dom'
 
 export const UpdateUser = () => {
     const id = useParams().id
+    const navigate = useNavigate()
+    
     //const {register,handleSubmit} =useForm({defaultValues:{name:"test",age:0,email:""}})
     const {register,handleSubmit} =useForm({defaultValues:async()=>{
       const res = await axios.get(`https://node5.onrender.com/user/user/${id}`)
       return res.data.data // {}
     }})
-    const submitHandler  =(data)=>{
-      console.log("data to updated,,,",data)
+    const submitHandler  = async(data)=>{
+      console.log("data to updated,,,",data) //id,name,email.age,isActive,password
+      //put -->update
+      delete data._id
+      const res = await axios.put(`https://node5.onrender.com/user/user/${id}`,data)
+      console.log(res) //api resonse...
+      if(res.status==200){
+        //alert toaster..
+        navigate("/apidemo1")
+      }
     }
   return (
     <div style={{textAlign:"center"}}>
@@ -29,6 +39,7 @@ export const UpdateUser = () => {
             <label>AGE</label>
             <input type='text' {...register("age")}></input>
           </div>
+          
 
           {/* take other fileds */}
           <input type="submit" value ="update"></input>
